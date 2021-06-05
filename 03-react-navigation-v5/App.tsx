97cb01a2186cbbcd499e64f74e821881ad93c9cd
formegusto <formegusto@gmail.com>
2021-06-06 08:10:17 +0900

    react-native-navigator

---
+++ 03-react-navigation-v5/App.tsx
@@ -0,0 +1,9 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { Text, View } from "react-native";
+
+function App() {
+  return <View></View>;
+}
+
+export default App;
